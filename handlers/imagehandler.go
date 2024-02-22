f2fed80150a7e26d57a8401ae4f2b0852be0229b
randhipp-flip <randhi.putra@flip.id>
2024-02-22 21:21:51 +0700

    add image downloader and jpeg validation

--- handlers/imagehandler.go
+++ handlers/imagehandler.go
@@ -3,6 +3,11 @@ package handlers
 import (
 	"boilerplate/database"
 	"boilerplate/models"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/utils"
@@ -19,10 +24,19 @@ func HueEdit(c *fiber.Ctx) error {
 	}
 	database.Insert(image)
 
-	return c.JSON(fiber.Map{
-		"success": true,
-		"image":   image,
-	})
+	filetype := FileDownloader(image.Url)
+	switch filetype {
+	case "image/jpeg", "image/jpg":
+		return c.JSON(fiber.Map{
+			"success": true,
+			"image":   image,
+		})
+	default:
+		return c.Status(fiber.ErrBadRequest.Code).JSON(fiber.Map{
+			"success":         false,
+			"responseMessage": "image file not supported",
+		})
+	}
 }
 
 // NotFound returns custom 404 page
@@ -37,4 +51,53 @@ func ImageList(c *fiber.Ctx) error {
 		"success": true,
 		"images":  images,
 	})
+}
+
+func FileDownloader(url string) string {
+	path := url
+	fname := filepath.Base(path)
+
+	f, err := os.Create("./data/images/inputs/" + fname)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	res, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	_, err = f.ReadFrom(res.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("image downloaded")
+
+	file, err := os.Open("./data/images/inputs/" + fname)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	buff := make([]byte, 512) // why 512 bytes ? see http://golang.org/pkg/net/http/#DetectContentType
+	_, err = file.Read(buff)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	filetype := http.DetectContentType(buff)
+
+	fmt.Println(filetype)
+
+	return filetype
 }
